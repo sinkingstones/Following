eb82b4dc5624fe283932dcb77ed8ea537485d862
Lemoo <ifibercc@gmail.com>
2017-02-17 23:53:16 +0800

    修改ME

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 //app.js
 App({
     onLaunch: function () {
-        console.log('App Launch')
     },
     onShow: function () {
-        console.log('App Show')
     },
     onHide: function () {
-        console.log('App Hide')
     },
     getUserInfo: function (cb) {
         var that = this
